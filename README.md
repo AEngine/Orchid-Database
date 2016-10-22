0efdc2bdc5e8ae2df7942619b40ec6f17cabcfc5
Aleksey Ilyin <alksily@outlook.com>
2016-10-22 23:03:09 +0300

    init repo

---
+++ README.md
@@ -0,0 +1,12 @@
+Orchid Database
+====
+It allows you to connect the database to a project using a wrapper around the PDO.
+
+#### Requirements
+* Orchid Framework
+* PHP >= 7.0
+
+#### Installation
+Run the following command in the root directory of your web project:
+  
+> `composer require aengine/orchid-database`
---
+++ src/Db.php
@@ -0,0 +1,145 @@
+<?php
+
+use AEngine\Database\Exception\DatabaseException;
+
+/**
+ * Db is wrap around PDO with support Master-Slave random select server
+ */
+class Db
+{
+    /**
+     * Array of connections
+     *
+     * @var array
+     */
+    protected static $connection = [];
+
+    /**
+     * @var PDO
+     */
+    protected static $lastConnection;
+
+    /**
+     * @var PDOStatement
+     */
+    public static $lastQuery;
+
+    /**
+     * Setup the Database
+     *
+     * @param array $configs
+     *
+     * @throws RuntimeException
+     * @throws DatabaseException
+     */
+    public static function setup(array $configs = [])
+    {
+        if ($configs) {
+            $default = [
+                'dsn'      => '',
+                'username' => '',
+                'password' => '',
+                'options'  => [],
+                'role'     => 'master',
+            ];
+
+            foreach ($configs as $index => $config) {
+                $config = array_merge($default, $config);
+
+                static::$connection[$config['role'] == 'master' ? 'master' : 'slave'][] = function () use ($config) {
+                    try {
+                        return new PDO(
+                            $config['dsn'],
+                            $config['username'],
+                            $config['password'],
+                            $config['options']
+                        );
+                    } catch (PDOException $ex) {
+                        throw new DatabaseException(
+                            'The connection to the database server fails (' . $ex->getMessage() . ')',
+                            0,
+                            $ex
+                        );
+                    }
+                };
+            }
+
+            return;
+        }
+
+        throw new RuntimeException('There are no settings to connect to the memory');
+    }
+
+    /**
+     * Returns PDO object
+     *
+     * @param bool $use_master
+     *
+     * @return PDO
+     * @throws UnexpectedValueException
+     */
+    public static function getConnection($use_master = false)
+    {
+        $pool = [];
+        $role = $use_master ? 'master' : 'slave';
+
+        switch (true) {
+            case !empty(static::$connection[$role]): {
+                $pool = static::$connection[$role];
+                break;
+            }
+            case !empty(static::$connection['master']): {
+                $pool = static::$connection['master'];
+                $role = 'master';
+                break;
+            }
+            case !empty(static::$connection['slave']): {
+                $pool = static::$connection['slave'];
+                $role = 'slave';
+                break;
+            }
+        }
+
+        if ($pool) {
+            if (is_array($pool)) {
+                return static::$connection[$role] = $pool[array_rand($pool)]();
+            } else {
+                return $pool;
+            }
+        }
+
+        throw new UnexpectedValueException('Unable to establish connection');
+    }
+
+    /**
+     * Prepares and executes a database query
+     *
+     * @param string $query
+     * @param array  $params
+     * @param bool   $use_master
+     *
+     * @return PDOStatement
+     */
+    public static function query($query, array $params = [], $use_master = false)
+    {
+        // obtain connection
+        static::$lastConnection = static::getConnection(
+            !$use_master ? (strncmp($query, 'SELECT', 6) || strncmp($query, '(SELECT', 6)) : true
+        );
+
+        static::$lastQuery = static::$lastConnection->prepare($query);
+        static::$lastQuery->execute($params);
+
+        return static::$lastQuery;
+    }
+
+    /**
+     * Returns the ID of the last inserted row
+     *
+     * @return string
+     */
+    public static function lastInsertId()
+    {
+        return static::$lastConnection->lastInsertId();
+    }
+}
